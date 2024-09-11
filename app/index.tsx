4ae34cd7ea543fea8068d938548d690a800a9fa1
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-09-11 23:48:49 +0800

    fix landing text

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
-import { View } from 'react-native';
-import { Text } from '~/components/ui/text';
-import { Link } from 'expo-router';
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
+import { Link } from "expo-router";
 
 export default function Screen() {
-
   return (
-    <View className='flex-1 justify-center items-center gap-5 p-6 bg-secondary/30'>
-      <Text className='text-3xl'>Emergeny Response</Text>
-      <Text className='text-md'>Sign in to get started.</Text>
-      <Link href="./(auth)/sign-in">Sign in</Link>
+    <View className="flex-1 justify-center items-center gap-5 p-6 bg-secondary/30">
+      <Text className="text-3xl">Emergeny Response</Text>
+      <Text className="text-md">Sign in to get started.</Text>
+      <Link href="./(auth)/sign-in">
+        <Text>Sign in</Text>
+      </Link>
     </View>
   );
 }
