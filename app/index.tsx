b59fb502db3cc00132c330bccedccd41432ab2bb
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-09-14 21:02:21 +0800

    fix: android padding

--- app/index.tsx
+++ app/index.tsx
@@ -15,23 +15,21 @@ export default function Screen() {
           // className="w-[120] h-[120]"
           style={{ width: 200, height: 200 }}
         />
-        <Text className="text-red-800 font-semibold text-lg px-5 text-center">
+        <Text className="text-red-800 font-semibold text-xl px-5 text-center">
           Ready to assist in times of need, whenever you are.
         </Text>
         <Link
           href="./(auth)/sign-in"
-          className="w-full border border-primary bg-primary rounded-lg p-5 items-center flex justify-center overflow-hidden"
+          className="w-full border text-center border-primary bg-primary rounded-lg p-5 items-center flex justify-center overflow-hidden"
         >
-          <Text className="text-xl text-center text-white">
-            Sign in to Get Started
-          </Text>
+          <Text className="text-xl  text-white">Sign in to Get Started</Text>
         </Link>
 
         <Link
           href="./(auth)/sign-up"
-          className="w-full border border-primary rounded-lg p-5 items-center flex justify-center overflow-hidden"
+          className="w-full border text-center border-primary rounded-lg p-5 items-center flex justify-center overflow-hidden"
         >
-          <Text className="text-xl text-center text-primary">Sign up</Text>
+          <Text className="text-xl  text-primary">Sign up</Text>
         </Link>
       </View>
     </ScrollView>
